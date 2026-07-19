0d6801926090abe5ed08bb93e6b4b56e5fc213c6
cmgoodquist <cmgoodquist@protonmail.com>
2026-07-18 21:54:02 -0500

    Functionality - implement a themed button hard-coded to accept one of three specific themes

---
+++ projects/themed-button/src/lib/themed-button.enum.ts
@@ -0,0 +1,5 @@
+export enum ButtonType {
+    implementation = "Implement",
+    warning = "Warn",
+    success = "Succeed"
+}
--- projects/themed-button/src/lib/themed-button.ts
+++ projects/themed-button/src/lib/themed-button.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, Input } from "@angular/core";
+import { ButtonType } from "./themed-button.enum";
 
 @Component({
-  selector: 'lib-themed-button',
-  imports: [],
-  template: ` <p>themed-button works!</p> `,
-  styles: ``,
+    selector: '[themed-button]',
+    templateUrl: './themed-button.html',
+    styleUrls: ['./themed-button.scss'],
+    standalone: true
 })
-export class ThemedButton {}
+
+export class ThemedButton {
+    @Input() type: ButtonType = ButtonType.implementation;
+
+    static colorMapping: Record<ButtonType, string> = {
+        [ButtonType.implementation]: "rgb(0,45,230)",
+        [ButtonType.warning]: "rgb(230, 45, 0)",
+        [ButtonType.success]: "rgb(90, 200, 0)"
+    };
+    static clickColorMapping: Record<ButtonType, string> = {
+        [ButtonType.implementation]: "rgb(180, 215, 255)",
+        [ButtonType.warning]: "rgb(255, 209, 209)",
+        [ButtonType.success]: "rgb(209, 255, 209)"
+    };
+
+    @HostBinding('style.--color') color = ThemedButton.colorMapping[this.type];
+    @HostBinding('style.--click-color') clickColor = ThemedButton.clickColorMapping[this.type];
+}
